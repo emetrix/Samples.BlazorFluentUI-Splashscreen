084eb95f20918dcf05cb3a286e74ba206867d40c
Tobias Schaller <tschaller@emetrix.de>
2024-04-05 21:56:32 +0200

    Initial project based on template "Fluent Blazor Web App" version 4.6.0

---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+using Samples.BlazorFluentUI_Splashscreen.Components;
+
+namespace Samples.BlazorFluentUI_Splashscreen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            builder.Services.AddFluentUIComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
